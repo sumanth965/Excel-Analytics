50421d243f69a88d89797679af832ab94572b568
Sumanth <sumanthpoojary965@gmail.com>
2025-07-07 18:25:15 +0530

    Updated App.jsx with latest changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,6 +17,8 @@ function App() {
           <Route path="/" element={<Auth />} />
           {/* <Route path="/l" element={<Login />} /> */}
           {/* <Route path="/register" element={<Register />} /> */}
+          {/* <Route path="/register" element={<Register />} /> */}
+          {/* <Route path="/register" element={<Register />} /> */}
           {/* ....................................... */}
           {/* ....................................... */}
         </Routes>
